b2192eee58af0b1431d71b95012c2216c47c9a6d
Guillermo Vera <vfguille@gmail.com>
2019-02-12 09:10:06 +0100

    Muestra bien la hora

--- app/src/main/java/com/example/broadcastalarmmanagerintent/MainActivity.java
+++ app/src/main/java/com/example/broadcastalarmmanagerintent/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.broadcastalarmmanagerintent;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edTime;
+    private String time;
+    private Button btConfirm;
+    private int REQUEST_CODE=100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,17 @@ public class MainActivity extends AppCompatActivity {
                 createTimePickerDialog();
             }
         });
+        btConfirm = findViewById(R.id.btConfirm);
+        btConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    setAlarmManager();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     /**
@@ -32,8 +56,27 @@ public class MainActivity extends AppCompatActivity {
         TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-
+                time = String.format("%02d:%02d", hourOfDay, minute, Locale.FRANCE);
+                setTimeAlarm(time);
             }
-        }, 0, 0, true);
+        }, Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE), true);
+        timePickerDialog.show();
+    }
+
+    private void setTimeAlarm(String time){
+        edTime.setText(time);
+    }
+
+    /**
+     * Método que establece una alarma en el sistema.
+     */
+    private void setAlarmManager() throws ParseException {
+        Date date = null;
+        Intent intent = new Intent("com.example.broadcastalarmmanagerintent.SHOW");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT); // Si se le da varias veces a establecer la larma, usa la última.
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm", Locale.FRANCE);
+        date = simpleDateFormat.parse(time);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, date.getTime(), pendingIntent);
     }
 }
